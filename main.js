edeb016aba74f79a114a44c098caba0fb3b88ff9
Aplha101 <dhruvbeastboy2020@gmail.com>
2022-01-22 23:28:52 +0530

    full game ig

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ let U = document.getElementById('up')
 let D = document.getElementById('down')
 let L = document.getElementById('left')
 let R = document.getElementById('right')
-let sc = 15
-let score = 0
+let scor = document.getElementById("sco")
+let sc = 10
+let score = 1
 area.style = "background:#556479;"
 area.height = html.getBoundingClientRect().width / 2
 area.width = html.getBoundingClientRect().width / 2
 let ctx = area.getContext('2d')
+
 
 let RandomNum = (max, min) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -19,61 +21,107 @@ function dist(x, y, posx, posy) {
   return Math.sqrt(Math.pow(x - posx, 2) + Math.pow(y - posy, 2))
 }
 
-let snake = {
-  x: area.width / 2,
-  y: area.height / 2,
-  xsp: 1,
-  ysp: 0,
-  dir: (x, y) => {
-    snake.xsp = x
-    snake.ysp = y
-  },
-  eat: (pos) => {
-    let a = dist(snake.x , snake.y , pos.x , pos.y)
-    if(a <= 10){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
-}
-
-  let food = {
-    x: Math.floor(RandomNum(area.width , 0)),
-    y: Math.floor(RandomNum(area.height , 0))
-  }
-function loc(npos){
-    npos.x = Math.floor(RandomNum(area.width , 0)),
-    npos.y = Math.floor(RandomNum(area.height , 0))
-}
-/* Canvas as a plane*/
-// ↑ -Y
-// ↓ +Y
-// → +X
-// ← -X
-
-let Draw = () => {
-  ctx.clearRect(0, 0, area.width, area.height)
-  ctx.fillStyle = "#fff"
-  ctx.beginPath()
-  ctx.rect(snake.x, snake.y, 10, 10)
-  ctx.fill()
-
-  ctx.fillStyle = "red"
-  ctx.beginPath()
-  ctx.rect(food.x, food.y, 10, 10)
-  ctx.fill()
-}
-
 function clamp(value, min, max) {
   return Math.min(Math.max(value, min), max);
 }
-setInterval(() => { //things happen here
-if(snake.eat(food)){
-  loc(food)
+
+function loc(npos) {
+  npos.x = Math.floor(RandomNum(area.width, 10))
+  npos.y = Math.floor(RandomNum(area.height, 10))
 }
-  Draw()
+
+
+class Snake {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+    this.xsp = 1
+    this.ysp = 0
+    this.tail = []
+    this.track = 0
+  }
+  eat(pos) {
+    let a = dist(this.x, this.y, pos.x, pos.y)
+    if (a <= 10) {
+      this.track += 10
+      return true;
+    } else {
+      return false;
+    }
+  }
+  death(){
+    for(let i = 0; i < this.tail.length; i++){
+      if(this.x == this.tail[i].x && this.y == this.tail[i].y){
+        alert(`GAME OVER , your score was : ${score}`)
+        this.tail = []
+        this.track = 0
+        score = 0
+        this.x = area.width/2
+        this.y = area.height/2 
+      }
+    }
+  }
+
+  update() {
+    for (let i = 0; i < this.tail.length - 1; i++) {
+      this.tail[i] = this.tail[i+1]
+    }
+    this.tail[this.track - 1] = { x: this.x, y: this.y }
+
+    this.x += this.xsp
+    this.y += this.ysp
+  }
+  dir(x, y) {
+    this.xsp = x
+    this.ysp = y
+  }
+  show() {
+    ctx.fillStyle = "#fff"
+    for (let i = 0; i < this.tail.length; i++){
+      ctx.fillRect(this.tail[i].x, this.tail[i].y, 10 , 10)
+    }
+    ctx.beginPath()
+    ctx.rect(snake.x, snake.y, 10, 10)
+    ctx.fill();
+  }
+  limit() {
+    this.x = clamp(snake.x, 0, area.width - sc)
+    this.y = clamp(snake.y, 0, area.height - sc)
+  }
+}
+
+class Food {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+  limit() {
+    food.x = clamp(food.x, 0, area.width - sc)
+    food.y = clamp(food.y, 0, area.height - sc)
+  }
+  show() {
+    ctx.fillStyle = "red"
+    ctx.beginPath()
+    ctx.rect(this.x, this.y, 10, 10)
+    ctx.fill();
+
+  }
+}
+
+
+let snake = new Snake(area.width / 2, area.height / 2)
+
+let food = new Food(Math.floor(RandomNum(area.width, 10)), Math.floor(RandomNum(area.height, 10)))
+
+
+
+let Clear = () => {
+  ctx.clearRect(0, 0, area.width, area.height)
+}
+
+
+function controls() {
+  //mobiles
   U.addEventListener("click", () => {
     snake.dir(0, -1)
   })
@@ -86,10 +134,35 @@ if(snake.eat(food)){
   R.addEventListener("click", () => {
     snake.dir(1, 0)
   })
- 
 
-  snake.x += snake.xsp
-  snake.y += snake.ysp
-  snake.x = clamp(snake.x, 0, area.width - sc)
-  snake.y = clamp(snake.y, 0, area.height - sc)
+  window.addEventListener("keydown", (e) => {
+    console.log(evt)
+  })
+
+}
+
+setInterval(() => {
+  Clear()
+  controls()
+
+  snake.update()
+  snake.show()
+  snake.limit()
+  snake.death()
+  
+  food.show()
+  food.limit()
+  
+  
+  if (snake.eat(food)) {
+    score++
+    loc(food)
+  }
+
 }, 17)
+
+//Canvas as a plane
+// ↑ -Y
+// ↓ +Y
+// → +X
+// ← -X
